263f207c5e1359f48a233637e541f6a1906fe71f
hit300b1 <hit300b1@gmail.com>
2022-04-18 10:31:06 +0530

    qq

---
+++ qptest2022/src/day2/AbstractDemo.java
@@ -0,0 +1,44 @@
+package day2;
+
+public class AbstractDemo {
+	public static void main(String[] args) {
+		Dominos dominos=new TuljhaDomino();
+		dominos.orderPizza();
+	}
+}
+
+abstract class Dominos{
+	final private void makeDough() {
+		System.out.println("making dough as per dominos...");
+	}
+	final private void addIngredients() {
+		System.out.println("adding ingredients as per dominos...");
+	}
+	final private void bakePizza() {
+		System.out.println("baking as per dominos...");
+	}
+	
+	final private void makePizza() {//template method
+		makeDough();
+		addIngredients();
+		bakePizza();
+	}
+	final public void orderPizza() {//template method
+		payment();
+		makePizza();
+		delivery();
+	}
+	public abstract void payment();
+	public abstract void delivery();
+}
+
+class TuljhaDomino extends Dominos{
+	@Override
+	public void delivery() {
+		System.out.println("delivery as per indian standards....tuljha will decide..");
+	}
+	@Override
+	public void payment() {
+		System.out.println("payment recd as per tuljha money handling...");
+	}
+}
